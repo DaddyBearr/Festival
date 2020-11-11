e6b7299405dc6896462bec74ec24e23bb2ce4e85
benjamingoureau <73234102+benjamingoureau@users.noreply.github.com>
2020-11-11 12:27:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$nom="Accueil";
+include "_debut.inc.php";
+?>
+<br> 
+<table width='80%' cellspacing='10' cellpadding='0' align='center'>
+   <tr>  
+      <td class='texteAccueil'>
+         Cette application web permet de gérer l'hébergement des groupes sportifs 
+         durant le festival Sp'Or.
+      </td>
+   </tr>
+   <tr>
+      <td>&nbsp;
+      </td>
+   </tr>
+   <tr>
+      <td class='texteAccueil'>
+          Elle offre les services suivants :
+      </td>
+   </tr>
+   <tr>
+      <td>&nbsp;
+      </td>
+   </tr>
+   <tr>
+      <td class='texteAccueil'>
+      <ul>
+         <li>Gérer les établissements : modifier les informations, ajouter des établissements ou encore supprimer des établissements  (caractéristiques et capacités d'accueil) acceptant d'héberger les groupes de sport.
+         <p>
+	      </p>
+         <li>Consulter, réaliser ou modifier les attributions des chambres aux groupes dans les établissements.
+      </ul>
+      </td>
+   </tr>
+</table>
+
+
